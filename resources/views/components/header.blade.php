532262e0e6081e3a8608adc70b6eb0a9b30f311e
ebarbozadev <e.barboza@edu.unipar.br>
2025-04-08 02:17:14 -0300

    Iniciando projeto e subindo escopo de header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<header id="headerPrincipal">
+    <div class="container">
+        <a href="/">
+            <img width="200" src="{{ asset('img/svg/logo.svg') }}" alt="Logo escrito 'Amazon Prime'">
+        </a>
+
+        <ul>
+            <li><a href="#">Novidades</a></li>
+            <li><a href="#">Diferenciais</a></li>
+            <li><a href="#">Depoimentos</a></li>
+            <li><a href="#">Produtos</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </div>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
+</head>
+
+<body>
+    <x-header />
+    <h1>Teste</h1>
+</body>
+
+</html>
